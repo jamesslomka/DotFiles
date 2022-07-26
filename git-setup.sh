60f8f08c40851db6c4484eb7e02c94c3bb1b5858
James Slomka <james.slomka@ssense.com>
2022-07-26 18:17:21 -0400

    update git setup script

--- git-setup.sh
+++ git-setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# General
+git config --global user.name "James Slomka"
+git config --global user.email james.slomka@gmail.com
+
+# Avoid needing to specify the upstream remote branch
+git config --global --add --bool push.autoSetupRemote true
+
 # Setup git pre push hook to prevent unwanted push to self-defined protected branches
 mkdir ~/.githooks
 cp scripts/pre-push ~/.githooks
